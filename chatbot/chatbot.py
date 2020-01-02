6391a9f7f0bbec07150c200c41a565f6c3629dc7
Johnny Chia <av_jchia@idcontractors.net>
2020-01-02 15:29:21 -0500

    add logic addapters

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -11,8 +11,23 @@ conversation = [
     "You're welcome."
 ]
 
-chatbot = ChatBot("Johnny Chia")
+chatbot = ChatBot(
+    "Johnny Chia",
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+)
 
 trainer = ListTrainer(chatbot)
 
 trainer.train(conversation)
+
+
+while True:
+    try:
+        bot_input = chatbot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
