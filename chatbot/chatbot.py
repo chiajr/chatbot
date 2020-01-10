cb058578cde12c076e2e46076bf9a9fa2858e2ea
Johnny Chia <av_jchia@idcontractors.net>
2020-01-10 11:46:38 -0500

    add preprocessors and some more logic adapters

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,33 +1,35 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
+from chatterbot.logic import SpecificResponseAdapter  # Edited this to statement.text
 
 chatbot = ChatBot(
     "Johnny Chia",
     logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter'
+        {
+            'import_path': 'chatterbot.logic.MathematicalEvaluation',
+        },
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+        },
+        {
+            'import_path': 'chatterbot.logic.TimeLogicAdapter',
+        },
+        {
+            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+            'input_text': 'Help me!',
+            'output_text': 'Ok, here is a link: http://chatterbot.rtfd.org'
+        }
     ],
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace',
+        'chatterbot.preprocessors.unescape_html',
+        'chatterbot.preprocessors.convert_to_ascii']
 )
-
-trainer = ListTrainer(chatbot)
-
-trainer.train(conversation)
-
 
 while True:
     try:
-        bot_input = chatbot.get_response(input())
-        print(bot_input)
+        user_input = input()
+        response = chatbot.get_response(user_input)
+        print(response)
 
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
