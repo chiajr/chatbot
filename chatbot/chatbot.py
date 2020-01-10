1fb2fb432594d062de732d890b56186c7c2c72d3
Johnny Chia <av_jchia@idcontractors.net>
2020-01-10 12:25:59 -0500

    update

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,35 +1,41 @@
 from chatterbot import ChatBot
-from chatterbot.logic import SpecificResponseAdapter  # Edited this to statement.text
 
-chatbot = ChatBot(
-    "Johnny Chia",
-    logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.MathematicalEvaluation',
-        },
-        {
-            'import_path': 'chatterbot.logic.BestMatch',
-        },
-        {
-            'import_path': 'chatterbot.logic.TimeLogicAdapter',
-        },
-        {
-            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
-            'input_text': 'Help me!',
-            'output_text': 'Ok, here is a link: http://chatterbot.rtfd.org'
-        }
-    ],
-    preprocessors=[
-        'chatterbot.preprocessors.clean_whitespace',
-        'chatterbot.preprocessors.unescape_html',
-        'chatterbot.preprocessors.convert_to_ascii']
-)
 
-while True:
-    try:
-        user_input = input()
-        response = chatbot.get_response(user_input)
-        print(response)
+def main_process():
 
-    except(KeyboardInterrupt, EOFError, SystemExit):
-        break
+    chatbot = ChatBot(
+        "Johnny Chia",
+        logic_adapters=[
+            {
+                'import_path': 'chatterbot.logic.MathematicalEvaluation',
+            },
+            {
+                'import_path': 'chatterbot.logic.BestMatch',
+            },
+            {
+                'import_path': 'chatterbot.logic.TimeLogicAdapter',
+            },
+            {
+                'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+                'input_text': 'Help me!',
+                'output_text': 'Ok, here is a link: http://chatterbot.rtfd.org'
+            }
+        ],
+        preprocessors=[
+            'chatterbot.preprocessors.clean_whitespace',
+            'chatterbot.preprocessors.unescape_html',
+            'chatterbot.preprocessors.convert_to_ascii']
+    )
+
+    while True:
+        try:
+            user_input = input()
+            response = chatbot.get_response(user_input)
+            print(response)
+
+        except(KeyboardInterrupt, EOFError, SystemExit):
+            break
+
+
+if __name__ == "__main__":
+    main_process()
